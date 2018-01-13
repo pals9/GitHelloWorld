5ce3ca5cdfb480849c2cc7251b4b5e4704571eae
Pal <soumava.pal@sap.com>
2018-01-13 17:48:18 +0100

    changes to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+		for (int i=1; i<=10; i++)
+        System.out.println("\nHello World.."+i);
     }
 
 }
